cb2ee9aba20a34007f4814127ee49b07b822dfc4
WilliamHuang <yilin.huang@mail.hypers.com>
2020-03-17 23:13:55 +0800

    feat(sass): support dark theme in css for browsers supporting css variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import Layout from '@/components/layout';
+// TODO: build error on production mode
+import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
+
+import './index.scss';
 
 const IndexPage = () => {
   let test = 1;
