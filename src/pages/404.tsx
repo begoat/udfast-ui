b3638db0847547f5574e3057cead0bf325e7611a
William Huang <yilin.huang@mail.hypers.com>
2020-03-19 08:37:55 +0800

    feat(404 page): wrap it a localeProvider

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { ReplaceComponentRendererArgs } from 'gatsby';
+
+import LocaleProvider from '@/components/LocaleProvider';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+const NotFoundPage = ({ pathContext }: ReplaceComponentRendererArgs) => {
+  const { locale } = pathContext as any;
+  return (
+    <LocaleProvider locale={locale}>
+      <Layout>
+        <SEO title="404: Not found" />
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Layout>
+    </LocaleProvider>
+  );
+};
 
 export default NotFoundPage;
