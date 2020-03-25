2f308337caac35ddfa137b7c628b0d1c989b0634
William Huang <yilin.huang@mail.hypers.com>
2020-03-25 08:20:15 +0800

    fix(import type runtime error): thanks to ts3.8 which enables import type only

--- src/utils/peer/wrapper.ts
+++ src/utils/peer/wrapper.ts
@@ -1,2 +1,6 @@
-export { UploadController } from 'udfast-core/lib/src/service/file-sharing/upload-side/controller/UploadController.d';
-export { DController } from 'udfast-core/lib/src/service/file-sharing/download-side/controller/DController.d';
+/**
+ * Thanks to TypeScript3.8
+ * https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html#-type-only-imports-and-export
+ */
+export type { DController } from 'udfast-core/lib/src/service/file-sharing/download-side/controller/DController.d';
+export type { UploadController } from 'udfast-core/lib/src/service/file-sharing/upload-side/controller/UploadController.d';
