c5b6db31fe2850bcc7eadaea00504168f24005a3
William Huang <hylbegoat@gmail.com>
2020-04-09 01:01:10 +0000

    Updates

---
+++ component---src-pages-404-tsx-c464285093880766ba88.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),c=a("xC1/"),o=a("Ac47"),r=a("dsgd");t.default=function(e){var t=e.pathContext.locale;return l.a.createElement(c.a,{locale:t},l.a.createElement(o.a,{titleKey:"404"},l.a.createElement(r.a,{titleKey:"404"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-c464285093880766ba88.js.map
